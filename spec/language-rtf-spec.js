7a6d1b837945af09e1b86f3a2ed26bc688ccaaff
Alhadis <gardnerjohng@gmail.com>
2025-02-18 15:13:19 +1100

    Use conventional scope-names for `begin`/`end` rules

--- spec/language-rtf-spec.js
+++ spec/language-rtf-spec.js
@@ -19,11 +19,11 @@ describe("language-rtf", () => {
     it("tokenizes groups", () => {
       const {tokens} = grammar.tokenizeLine("{}{\\*\\foo}");
       expect(tokens.length).toBe(5);
-      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
-      expect(tokens[1]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
-      expect(tokens[2]).toEqual({value: "{\\*", scopes: ["text.rtf", "keyword.operator.begin-ignorable-destination-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
+      expect(tokens[1]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
+      expect(tokens[2]).toEqual({value: "{\\*", scopes: ["text.rtf", "keyword.operator.group.ignorable-destination.begin.rtf"]});
       expect(tokens[3]).toEqual({value: "\\foo", scopes: ["text.rtf", "support.function.rtf"]});
-      expect(tokens[4]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-ignorable-destination-group.rtf"]});
+      expect(tokens[4]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.ignorable-destination.end.rtf"]});
     });
 
     it("tokenizes control symbols", () => {
@@ -51,32 +51,32 @@ describe("language-rtf", () => {
       `);
       let tokens = lines[0];
       expect(tokens.length).toBe(14);
-      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[1]).toEqual({value: "\\rtf", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[2]).toEqual({value: "1", scopes: ["text.rtf", "constant.numeric.rtf"]});
       expect(tokens[3]).toEqual({value: "\\ansi", scopes: ["text.rtf", "support.function.rtf"]});
-      expect(tokens[4]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[4]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[5]).toEqual({value: "\\fonttbl", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[6]).toEqual({value: "\\f", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[7]).toEqual({value: "0", scopes: ["text.rtf", "constant.numeric.rtf"]});
       expect(tokens[8]).toEqual({value: "\\fswiss", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[9]).toEqual({value: " Helvetica;", scopes: ["text.rtf"]});
-      expect(tokens[10]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[10]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
       expect(tokens[11]).toEqual({value: "\\f", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[12]).toEqual({value: "0", scopes: ["text.rtf", "constant.numeric.rtf"]});
       expect(tokens[13]).toEqual({value: "\\pard", scopes: ["text.rtf", "support.function.rtf"]});
       tokens = lines[1];
       expect(tokens.length).toBe(7);
       expect(tokens[0]).toEqual({value: "This is some ", scopes: ["text.rtf"]});
-      expect(tokens[1]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[1]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[2]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[3]).toEqual({value: " bold", scopes: ["text.rtf", "markup.bold.rtf"]});
-      expect(tokens[4]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[4]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
       expect(tokens[5]).toEqual({value: " text.", scopes: ["text.rtf"]});
       expect(tokens[6]).toEqual({value: "\\par", scopes: ["text.rtf", "support.function.rtf"]});
       tokens = lines[2];
       expect(tokens.length).toBe(1);
-      expect(tokens[0]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
     });
 
     it("tokenizes property changes", () => {
@@ -89,7 +89,7 @@ describe("language-rtf", () => {
       `);
       let tokens = lines[0];
       expect(tokens.length).toBe(9);
-      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[1]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[2]).toEqual({value: " bold ", scopes: ["text.rtf", "markup.bold.rtf"]});
       expect(tokens[3]).toEqual({value: "\\i", scopes: ["text.rtf", "markup.bold.rtf", "support.function.rtf"]});
@@ -97,21 +97,21 @@ describe("language-rtf", () => {
       expect(tokens[5]).toEqual({value: "\\i", scopes: ["text.rtf", "markup.bold.rtf", "support.function.rtf"]});
       expect(tokens[6]).toEqual({value: "0", scopes: ["text.rtf", "markup.bold.rtf", "constant.numeric.rtf"]});
       expect(tokens[7]).toEqual({value: " Bold again", scopes: ["text.rtf", "markup.bold.rtf"]});
-      expect(tokens[8]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[8]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
       tokens = lines[1];
       expect(tokens.length).toBe(9);
-      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[1]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[2]).toEqual({value: " bold ", scopes: ["text.rtf", "markup.bold.rtf"]});
-      expect(tokens[3]).toEqual({value: "{", scopes: ["text.rtf", "markup.bold.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[3]).toEqual({value: "{", scopes: ["text.rtf", "markup.bold.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[4]).toEqual({value: "\\i", scopes: ["text.rtf", "markup.bold.rtf", "support.function.rtf"]});
       expect(tokens[5]).toEqual({value: " Bold Italic ", scopes: ["text.rtf", "markup.bold.rtf", "markup.italic.rtf"]});
-      expect(tokens[6]).toEqual({value: "}", scopes: ["text.rtf", "markup.bold.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[6]).toEqual({value: "}", scopes: ["text.rtf", "markup.bold.rtf", "keyword.operator.group.end.rtf"]});
       expect(tokens[7]).toEqual({value: "Bold again", scopes: ["text.rtf", "markup.bold.rtf"]});
-      expect(tokens[8]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[8]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
       tokens = lines[2];
       expect(tokens.length).toBe(9);
-      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[1]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[2]).toEqual({value: " bold ", scopes: ["text.rtf", "markup.bold.rtf"]});
       expect(tokens[3]).toEqual({value: "\\i", scopes: ["text.rtf", "markup.bold.rtf", "support.function.rtf"]});
@@ -119,20 +119,20 @@ describe("language-rtf", () => {
       expect(tokens[5]).toEqual({value: "\\plain", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[6]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[7]).toEqual({value: " Bold again", scopes: ["text.rtf", "markup.bold.rtf"]});
-      expect(tokens[8]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[8]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
     });
 
     it("tokenizes property changes without spaces", () => {
       // https://github.com/nwhetsell/language-rtf/issues/1
       const {tokens} = grammar.tokenizeLine("{\\b bold \\b0not bold}");
       expect(tokens.length).toBe(7);
-      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.begin-group.rtf"]});
+      expect(tokens[0]).toEqual({value: "{", scopes: ["text.rtf", "keyword.operator.group.begin.rtf"]});
       expect(tokens[1]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[2]).toEqual({value: " bold ", scopes: ["text.rtf", "markup.bold.rtf"]});
       expect(tokens[3]).toEqual({value: "\\b", scopes: ["text.rtf", "support.function.rtf"]});
       expect(tokens[4]).toEqual({value: "0", scopes: ["text.rtf", "constant.numeric.rtf"]});
       expect(tokens[5]).toEqual({value: "not bold", scopes: ["text.rtf"]});
-      expect(tokens[6]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.end-group.rtf"]});
+      expect(tokens[6]).toEqual({value: "}", scopes: ["text.rtf", "keyword.operator.group.end.rtf"]});
     });
   });
 });
